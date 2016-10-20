f549a6712342591768d1503d64b3bc670c6e8cf0
jasonhsieh <appleoxxo@gmail.com>
2016-10-20 14:59:48 +0800

    8th commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 app.enable('trust proxy');
 	
 app.get('/',function(req,res){
-	res.send('welcome to heroku');
+	res.sendFile(path.join(__dirname,'public','index.html'));
 });
+
+app.use(express.static(__dirname + '/public'));
 
 
 var port = process.env.PORT || 8080;
